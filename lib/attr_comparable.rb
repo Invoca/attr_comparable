667ca93440f167347aa5c3616e0d3db96e5f998f
Declan Kelly <dkelly@invoca.com>
2017-08-02 13:22:39 -0700

    TECH-1476 Upgrade to Ruby 2.4: Revamped attr_compare mixin to handle nil correctly

--- lib/attr_comparable.rb
+++ lib/attr_comparable.rb
@@ -3,38 +3,49 @@
 #
 # Use attr_comparabie <attribute list>
 # to declare the attributes which should be compared, and the order they should be
-# Attributes may be nil; nil attributes sort earlier than non-nil to match SQL NULL ordering
+# Attributes may be nil; nil attributes sort earlier than non-nil to match the SQL convention
 #
 module AttrComparable
   include Comparable
 
   module ClassMethods
     # like <=> but handles nil values
-    # returns the code in string form to be eval'd
-    def compare_with_nil_code(left, right)
-      <<-EOS
-        if #{ left }.nil?
-          if #{ right }.nil?
-            0
-          else
-            -1
-          end
-        elsif #{ right }.nil?
-          1
+    # when equal, returns nil rather than 0 so the caller can || together
+    def compare_with_nil(left, right)
+      if left.nil?
+        if right.nil?
+          nil
         else
-          #{ left } <=> #{ right }
+          -1
         end
-      EOS
+      elsif right.nil?
+        1
+      else
+        cmp_result = (safe_compare(left) <=> safe_compare(right))
+        return nil if cmp_result.nil?
+        cmp_result.nonzero?
+      end
+    end
+
+    def safe_compare(value)
+      case value
+        when TrueClass
+          [1, 1]
+        when FalseClass
+          [1, 0]
+        else
+          [0, value]
+      end
     end
 
     def attr_compare(*attributes)
-      attr_exprs = (attributes.flatten).map do |attribute|
-        '(' + compare_with_nil_code("self.#{attribute}", "rhs.#{attribute}").strip + ')'
-      end
-
+      attributes = attributes.flatten
       class_eval <<-EOS
         def <=>(rhs)
-          #{ attr_exprs.join(".nonzero? || ") }
+          #{attributes.map do |attribute|
+              "self.class.compare_with_nil(self.#{attribute}, rhs.#{attribute})"
+            end.join(" || ")
+           } || 0
         end
       EOS
     end
